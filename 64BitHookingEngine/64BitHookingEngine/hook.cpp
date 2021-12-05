76399cafae862d990b2c58b86516ee90fdec717d
lfontesm <moreirafontesleo@gmail.com>
2021-12-05 20:37:26 -0300

    huuuuge progress

--- 64BitHookingEngine/64BitHookingEngine/hook.cpp
+++ 64BitHookingEngine/64BitHookingEngine/hook.cpp
@@ -5,146 +5,198 @@
 #include "hook.h"
 #include "hde64.h"
 
+#define TRAMPOLINE_MAX_SIZE (64 - sizeof(JMP_ABS))
+
+
+#pragma once
+
+#pragma pack(push, 1)
+// 64-bit indirect absolute jump.
+typedef struct _JMP_ABS
+{
+    UINT8  opcode0;     // FF25 00000000: JMP [+6]
+    UINT8  opcode1;
+    UINT32 dummy;
+    UINT64 address;     // Absolute destination address
+} JMP_ABS, * PJMP_ABS;
+
+// 64-bit indirect absolute call.
+typedef struct _CALL_ABS
+{
+    UINT8  opcode0;     // FF15 00000002: CALL [+6]
+    UINT8  opcode1;
+    UINT32 dummy0;
+    UINT8  dummy1;      // EB 08:         JMP +10
+    UINT8  dummy2;
+    UINT64 address;     // Absolute destination address
+} CALL_ABS;
+
+typedef struct _JCC_ABS
+{
+    UINT8  opcode;      // 7* 0E:         J** +16
+    UINT8  dummy0;
+    UINT8  dummy1;      // FF25 00000000: JMP [+6]
+    UINT8  dummy2;
+    UINT32 dummy3;
+    UINT64 address;     // Absolute destination address
+} JCC_ABS;
+
+typedef struct _JMP_REL
+{
+    UINT8  opcode;      // E9/E8 xxxxxxxx: JMP/CALL +5+xxxxxxxx
+    UINT32 operand;     // Relative destination address
+} JMP_REL, * PJMP_REL, CALL_REL;
+#pragma pack(pop)
+
 // Use _InterlockedCompareExchange64 insted of inline ASM (depends on compiler)
 #define NO_INLINE_ASM
 #undef UNICODE
 
-TdefOldMessageBoxA OldMessageBoxA;
-TdefOldMessageBoxW OldMessageBoxW;
+// Size of each memory block. (= page size of VirtualAlloc)
+#define MEMORY_BLOCK_SIZE 0x1000
 
-LPVOID originalMemArea;
+// Max range for seeking a memory block. (= 1024MB)
+#define MAX_MEMORY_RANGE 0x40000000
 
-HOOK_ARRAY HookArray[] =
+typedef int (WINAPI* MESSAGEBOXA)(HWND, LPCSTR, LPCSTR, UINT);
+MESSAGEBOXA fpMessageBoxA = NULL;
+
+// Detour function which overrides MessageBoxA
+int WINAPI DetouMessageBoxA(HWND hWnd, LPCSTR lpText, LPCSTR lpCaption, UINT uType) {
+    return fpMessageBoxA(hWnd, "hooked successfuly", "no", uType);
+}
+
+static LPVOID FindPrevFreeRegion(LPVOID pAddress, LPVOID pMinAddr, DWORD dwAllocationGranularity)
 {
-	{"user32.dll", "MessageBoxA", (LPVOID)&NewMessageBoxA, &OldMessageBoxA, 0},
-	//{"user32.dll", "MessageBoxW", (LPVOID)&NewMessageBoxA, &OldMessageBoxW, 0},
-};
+    ULONG_PTR tryAddr = (ULONG_PTR)pAddress;
 
-//We need to copy 5 bytes, but we can only do 2, 4, 8 atomically
-//Pad buffer to 8 bytes then use lock cmpxchg8b instruction
-void SafeMemcpyPadded(LPVOID destination, LPVOID source, DWORD size) {
-	BYTE SourceBuffer[8];
+    // Round down to the allocation granularity.
+    tryAddr -= tryAddr % dwAllocationGranularity;
 
-	if (size > 8)
-		return;
+    // Start from the previous allocation granularity multiply.
+    tryAddr -= dwAllocationGranularity;
 
-	//Pad the source buffer with bytes from destination
-	memcpy(SourceBuffer, destination, 8);
-	memcpy(SourceBuffer, source, size);
+    while (tryAddr >= (ULONG_PTR)pMinAddr)
+    {
+        MEMORY_BASIC_INFORMATION mbi;
+        if (VirtualQuery((LPVOID)tryAddr, &mbi, sizeof(mbi)) == 0)
+            break;
 
-#ifndef NO_INLINE_ASM
-	__asm
-	{
-		lea esi, SourceBuffer;
-		mov edi, destination;
+        if (mbi.State == MEM_FREE)
+            return (LPVOID)tryAddr;
 
-		mov eax, [edi];
-		mov edx, [edi + 4];
-		mov ebx, [esi];
-		mov ecx, [esi + 4];
+        if ((ULONG_PTR)mbi.AllocationBase < dwAllocationGranularity)
+            break;
 
-		lock cmpxchg8b[edi];
-	}
-#else
-	_InterlockedCompareExchange64((LONGLONG*)destination, *(LONGLONG*)SourceBuffer, *(LONGLONG*)destination);
-#endif
+        tryAddr = (ULONG_PTR)mbi.AllocationBase - dwAllocationGranularity;
+    }
+
+    return NULL;
 }
 
-int WINAPI NewMessageBoxA(HWND hWnd, LPCSTR lpText, LPCTSTR lpCaption, UINT uType)
-{
-	printf("MessageBoxA called!\ntitle: %s\ntext: %s\n\n", lpCaption, lpText);
-	return OldMessageBoxA(hWnd, lpText, lpCaption, uType);
+// 32 bit relative jumps only cover a handful of addresses,
+// so we set a pre-determined range of addresses
+// to look for for writing the trampoline
+LPVOID findFreeRegion(LPVOID lpMemRegion) {
+    ULONG_PTR minAddr;
+    ULONG_PTR maxAddr;
+    LPVOID pOrigin = lpMemRegion;
+
+    SYSTEM_INFO si;
+    GetSystemInfo(&si);
+    minAddr = (ULONG_PTR)si.lpMinimumApplicationAddress;
+    maxAddr = (ULONG_PTR)si.lpMaximumApplicationAddress;
+
+    // pOrigin ± 512MB
+    if ((ULONG_PTR)pOrigin > MAX_MEMORY_RANGE && minAddr < (ULONG_PTR)pOrigin - MAX_MEMORY_RANGE)
+        minAddr = (ULONG_PTR)pOrigin - MAX_MEMORY_RANGE;
+
+    if (maxAddr > (ULONG_PTR)pOrigin + MAX_MEMORY_RANGE)
+        maxAddr = (ULONG_PTR)pOrigin + MAX_MEMORY_RANGE;
+
+    // Make room for MEMORY_BLOCK_SIZE bytes.
+    maxAddr -= MEMORY_BLOCK_SIZE - 1;
+
+    LPVOID pAlloc = pOrigin;
+    while ((ULONG_PTR)pAlloc >= minAddr) {
+        pAlloc = FindPrevFreeRegion(pAlloc, (LPVOID)minAddr, si.dwAllocationGranularity);
+        if (pAlloc == NULL)
+            break;
+
+        pAlloc = VirtualAlloc(
+            pAlloc, MEMORY_BLOCK_SIZE, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+        if (pAlloc != NULL)
+            break;
+    }
+
+    return pAlloc;
 }
 
-BOOL HookFunction(CHAR* dll, CHAR* name, LPVOID proxy, LPVOID original, PDWORD length) {
-	LPVOID funcAddr;
-	DWORD trampolineLength = 0, originalProtection;
-	hde64s disasm;
-	BYTE jump[9]{ 0xe9, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0 };
+LPBYTE relay;
+BOOL createTrampoline(LPVOID target, LPVOID detour, LPVOID trampoline, LPVOID *original) {
+    CALL_ABS call = {
+        0xFF, 0x15, 0x00000002, // FF15 00000002: CALL [RIP+8]
+        0xEB, 0x08,             // EB 08:         JMP +10
+        0x0000000000000000ULL   // Absolute destination address
+    };
+    JMP_ABS jmp = {
+        0xFF, 0x25, 0x00000000, // FF25 00000000: JMP [RIP+6]
+        0x0000000000000000ULL   // Absolute destination address
+    };
+    JCC_ABS jcc = {
+        0x70, 0x0E,             // 7* 0E:         J** +16
+        0xFF, 0x25, 0x00000000, // FF25 00000000: JMP [RIP+6]
+        0x0000000000000000ULL   // Absolute destination address
+    };
 
-	funcAddr = GetProcAddress(GetModuleHandleA(dll), name);
-	if (!funcAddr) {
-		printf("Failed to hook %s\n", name);
-		return false;
-	}
+    DWORD trampolineLength = 0, originalProtection;
 
-	// Disassemble length of each instruction, until we have 9 or more bytes worth
-	while (trampolineLength < 9) {
-		LPVOID instPointer = (LPVOID)((DWORD)funcAddr + trampolineLength);
-		trampolineLength += hde64_disasm(instPointer, &disasm);
-	}
+    hde64s disasm;
+    printf("aa %d\n",sizeof(JMP_REL));
+    while (trampolineLength < sizeof(JMP_REL)) {
+        LPVOID instructionPointer = (LPVOID)((DWORD64)target + trampolineLength);
+        trampolineLength += hde64_disasm(instructionPointer, &disasm);
+    }
+    __movsb((LPBYTE)trampoline, (LPBYTE)target, trampolineLength);
 
-	// Build the trampoline buffer
-	memcpy(original, funcAddr, trampolineLength);
-	*(DWORD*)(jump + 1) = ((DWORD)funcAddr + trampolineLength) - ((DWORD)original + trampolineLength + 9);
-	memcpy((LPVOID)((DWORD)original + trampolineLength), jump, 9);
+    jmp.address = (ULONG_PTR)target + trampolineLength;
+    LPVOID    pCopySrc = &jmp;
+    UINT      copySize = sizeof(jmp);
 
-	// Make sure the function is writable
-	if (!VirtualProtect(funcAddr, trampolineLength, PAGE_EXECUTE_READWRITE, &originalProtection)) {
-		printf("Failed to change memory protection for %s\n", name);
-		return false;
-	}
+    __movsb((LPBYTE)trampoline + trampolineLength, (LPBYTE)pCopySrc, copySize);
 
-	// Build and atomically write the hook
-	*(DWORD*)(jump + 1) = (DWORD)proxy - (DWORD)funcAddr - 9;
-	SafeMemcpyPadded(funcAddr, jump, 9);
+    jmp.address = (ULONG_PTR)detour;
+    UINT8 relayPos = trampolineLength + copySize;
+    relay = (LPBYTE)trampoline + relayPos;
 
-	// Restore the original page protection
-	VirtualProtect(funcAddr, trampolineLength, originalProtection, &originalProtection);
+    memcpy(relay, &jmp, sizeof(jmp));
 
-	// Clear CPU instruction cache
-	FlushInstructionCache(GetCurrentProcess(), funcAddr, trampolineLength);
+    DWORD oldProtect;
+    if (!VirtualProtect(target, 0x1000, PAGE_EXECUTE_READWRITE, &oldProtect)) {
+        printf("bery vad\n");
+        exit(1);
+    }
 
-	*length = trampolineLength;
+    PJMP_REL pJmp = (PJMP_REL)target;
+    pJmp->opcode = 0xE9;
+    pJmp->operand = (UINT32)((LPBYTE)relay - ((LPBYTE)target + sizeof(JMP_REL)));
 
-	return true;
+    VirtualProtect(target, 0x1000, oldProtect, &oldProtect);
+
+    FlushInstructionCache(GetCurrentProcess(), target, sizeof(JMP_REL));
+
+    *original= trampoline;
+
+    return NULL;
 }
-
-BOOL UnhookFunction(CHAR* dll, CHAR* name, LPVOID original, DWORD length) {
-	LPVOID funcAddr;
-	DWORD originalProtect;
-
-	funcAddr = GetProcAddress(GetModuleHandleA(dll), name);
-	if (!funcAddr) {
-		printf("Unexpected error while unhooking %s\n", name);
-		return false;
-	}
-
-	if (!VirtualProtect(funcAddr, length, PAGE_EXECUTE_READWRITE, &originalProtect)) {
-		printf("Failed to change memory protection of %s\n");
-		return false;
-	}
-
-	SafeMemcpyPadded(funcAddr, original, length);
-
-	VirtualProtect(funcAddr, length, originalProtect, &originalProtect);
-
-	FlushInstructionCache(GetCurrentProcess(), funcAddr, length);
-
-	return true;
-}
-
-void HookAll() {
-	int NumEntries = sizeof(HookArray) / sizeof(HOOK_ARRAY);
-
-	// Needs 25 bytes for each hooked function to hold original byte + return jump
-	originalMemArea = VirtualAlloc(NULL, 25 * NumEntries, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-	if (!originalMemArea) {
-		printf("Failed to allocated mem for storing original functions");
-		exit(1);
-	}
-
-	for (int i = 0; i < NumEntries; i++) {
-		// Split allocated memory into a block of 29 bytes for each hooked function
-		*(LPVOID*)HookArray[i].original = (LPVOID)((DWORD)originalMemArea + (i * 29));
-		HookFunction((char *)HookArray[i].dll, (char *)HookArray[i].name, *(LPVOID*)HookArray[i].original, &HookArray[i].length);
-	}
-}
-
-
 
 int main() {
-	HookAll();
-	
+    
+    LPVOID freeRegion = findFreeRegion(&MessageBoxA);
+    createTrampoline(MessageBoxA, DetouMessageBoxA, freeRegion, reinterpret_cast<LPVOID*>(&fpMessageBoxA));
+
+    printf("Found this free region: %llx\n", freeRegion);
+
 	MessageBoxA(NULL, "Testing", "Hook", MB_OK);
+    return 0;
 }
